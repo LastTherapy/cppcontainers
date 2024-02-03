86ae02adc4479afac1411e8c79f0a1d22f0f0e72
sageaure <sageaure@student.21-school.ru>
2024-02-03 07:42:16 +0300

    to 2-bound

--- src/lib/list.h
+++ src/lib/list.h
@@ -14,8 +14,9 @@ public:
   typedef struct node {
     T data;
     node *next;
+    node *prew;
     // Конструктор узла для удобства
-    node(const T &d, node *nd = nullptr) : data(d), next(nd) {}
+    node(const T &d, node *nx = nullptr, node *pw = nullptr) : data(d), next(nx), prew(pw) {}
   } node;
 
 public:
@@ -23,13 +24,14 @@ public:
   list() : head(nullptr), tail(nullptr), count(0) {}
 
   // создаем список с n пустыми элементами
-  list(size_t n) : head(nullptr), tail(nullptr), count(0) {
+  list(size_type n) : head(nullptr), tail(nullptr), count(0) {
     if (n == 0)
       return;
     head = new node(T());
     node *p = head;
-    for (size_t i = 1; i < n; i++) {
+    for (size_type i = 1; i < n; i++) {
       p->next = new node(T());
+      p->next->prew = p;
       p = p->next;
     }
     tail = p;
@@ -45,15 +47,12 @@ public:
     node *p = head;
     for (++it; it != items.end(); ++it) {
       p->next = new node(*it, nullptr);
+      p->next->prew = p;
       p = p->next;
     }
     tail = p;
     count = items.size();
   }
-
-  
-
- 
 
   // --------I T E R A T O R------------------------------------------ //
 
@@ -75,8 +74,19 @@ public:
       return temp;
     }
 
-    bool operator==(ListIterator &other) { return current == other.current; }
-    bool operator!=(const ListIterator &other) {
+    ListIterator &operator--() {  
+    }
+
+    ListIterator operator--(int) {
+      ListIterator temp = *this;
+      --(*this);
+      return temp;
+    }
+
+    bool operator==(const ListIterator &other) const {
+      return current == other.current;
+    }
+    bool operator!=(const ListIterator &other) const {
       return current != other.current;
     }
 
@@ -96,25 +106,87 @@ public:
   iterator end() { return iterator(nullptr); }
 
   // List Capacity
-  bool empty();
-  size_type size();
-  size_type max_size();
+  bool empty() { return count == 0 ? true : false; }
+  size_type size() { return count; }
+  size_type max_size() { return count; }
 
   // List Modifiers
-  void clear();
-  iterator insert(iterator pos, const_reference value);
-  void erase(iterator pos);
-  void push_back(const_reference value);
-  void pop_back();
-  void push_front(const_reference value);
-  void pop_front();
-  void swap(list &other);
-  void merge(list &other);
-  void splice(const_iterator pos, list &other);
-  void reverse();
-  void unique();
-  void sort();
+  void clear() {
+    node *current = head;
+    while (current != nullptr) {
+      node *next = current->next;
+      delete current;
+      current = next;
+    }
+    head = nullptr;
+    tail = nullptr;
+    count = 0;
+  }
+  iterator insert(iterator pos, const_reference value) {
+    if (pos == begin()) {
+      push_front(value); // +count in push front
+      return begin();
+    } else if (pos == end()) {
+      push_back(value);
+      return iterator(tail);
+    } else {
+      node *current = pos.current;
+      node *temp_prew  = current->prew;
+      current->prew = new node(value, current, temp_prew);
+      count++;
+      return (iterator(current->prew));
+      // node *current = head;
+      // while (current != nullptr && iterator(current->next) != pos)
+      //   current = current->next;
 
+      // if (current) {
+      //   node *temp = current->next;
+      //   current->next = new node(value, temp);
+      //   count++;
+      //   return iterator(current->next);
+      // } else {
+      //   return end();
+      // }
+    }
+  }
+
+  void erase(iterator pos) {}
+
+  void push_back(const_reference value) {
+    node *new_node = new node(value);
+    if (head == nullptr) {
+      head = new_node;
+      tail = new_node;
+    } else {
+      tail->next = new node(value);
+      tail = tail->next;
+    }
+    count++;
+  }
+
+  void pop_back() {}
+
+  void push_front(const_reference value) {
+    node *temp = head;
+    head = new node(value);
+    head->next = temp;
+    if (temp == nullptr) {
+      tail = head;
+    }
+    count++;
+  }
+
+  void pop_front() {}
+  void swap(list &other) {}
+  void merge(list &other) {}
+  void splice(const_iterator pos, list &other) {}
+  void reverse() {}
+  void unique() {}
+  void sort() {}
+
+node *getHead()  {
+    return head;
+}
 private:
   node *head;
   node *tail;
---
+++ src/test.cpp
@@ -0,0 +1,196 @@
+#include <gtest/gtest.h>
+#include "lib/list.h" // Подключение вашего файла заголовка списка
+
+// Тест на проверку создания пустого списка
+TEST(ListTest, CreatesEmptyList) {
+    list<int> my_list;
+    EXPECT_TRUE(my_list.empty());
+    EXPECT_EQ(0, my_list.size());
+}
+
+// Тест на добавление элементов в список
+TEST(ListTest, CanAddItems) {
+    list<int> my_list;
+    my_list.push_back(1); // Предполагается, что у вас есть такой метод
+    my_list.push_back(2);
+
+    EXPECT_FALSE(my_list.empty());
+    EXPECT_EQ(2, my_list.size());
+}
+
+// Тест на корректность итерации по списку
+TEST(ListTest, CanIterateList) {
+    list<int> my_list{1, 2, 3}; // Инициализация с помощью списка инициализации
+    int sum = 0;
+    for (auto it = my_list.begin(); it != my_list.end(); ++it) {
+        sum += *it;
+    }
+    EXPECT_EQ(6, sum); // 1+2+3 должно быть равно 6
+}
+
+// Тест на корректность front и back
+TEST(ListTest, CorrectFrontAndBack) {
+    list<int> my_list{1, 2, 3, 4};
+    EXPECT_EQ(1, my_list.front());
+    EXPECT_EQ(4, my_list.back());
+}
+
+TEST(ListTest, ClearsList) {
+    list<int> my_list{1, 2, 3};
+    my_list.clear();
+    EXPECT_TRUE(my_list.empty());
+}
+
+// Тест на добавление элементов в произвольное место списка
+TEST(ListTest, InsertsElements) {
+    list<int> my_list{1, 3, 4};
+    auto it = my_list.begin();
+    ++it; // Указываем на элемент со значением 3
+    my_list.insert(it, 2); // Вставляем 2 перед 3
+
+    // Проверяем, что элементы расположены в правильном порядке
+    int expected[] = {1, 2, 3, 4};
+    int i = 0;
+    for (auto elem : my_list) {
+        EXPECT_EQ(expected[i++], elem);
+    }
+}
+
+// Тест на вставку в начало списка
+TEST(ListTest, InsertAtBegin) {
+    list<int> my_list{2, 3, 4};
+    auto it = my_list.insert(my_list.begin(), 1); // Вставляем 1 в начало списка
+    EXPECT_EQ(1, *my_list.begin());  // Проверяем, что первый элемент теперь 1
+    EXPECT_EQ(1, *it);  // Проверяем, что возвращаемый итератор указывает на новый элемент
+}
+
+// Тест на вставку в конец списка
+TEST(ListTest, InsertAtEnd) {
+    list<int> my_list{1, 2, 3};
+    auto it = my_list.insert(my_list.end(), 4); // Вставляем 4 в конец списка
+    EXPECT_EQ(4, *--my_list.end());  // Проверяем, что последний элемент теперь 4
+    EXPECT_EQ(4, *it);  // Проверяем, что возвращаемый итератор указывает на новый элемент
+}
+
+// Тест на вставку в середину списка
+TEST(ListTest, InsertInMiddle) {
+    list<int> my_list{1, 3, 4};
+    auto it = my_list.begin();
+    ++it; // Указываем на элемент со значением 3
+    it = my_list.insert(it, 2); // Вставляем 2 перед 3
+
+    // Проверяем, что вставка прошла успешно
+    EXPECT_EQ(2, *it);  // Вставленный элемент должен быть 2
+    ++it;
+    EXPECT_EQ(3, *it);  // Следующий элемент должен быть 3
+}
+
+// Тест на вставку с использованием range-based for loop
+// TEST(ListTest, InsertUsingRangeFor) {
+//     list<int> my_list{1, 2, 4, 5};
+//     auto it = my_list.begin();
+//     std::advance(it, 2); // Перемещаем итератор на позицию перед которой будет вставка
+//     it = my_list.insert(it, 3); // Вставляем 3 перед 4
+
+//     int expected[] = {1, 2, 3, 4, 5};
+//     int i = 0;
+//     for (int val : my_list) {
+//         EXPECT_EQ(expected[i++], val);
+//     }
+// }
+
+// Тест на вставку в пустой список
+TEST(ListTest, InsertInEmptyList) {
+    list<int> my_list;
+    auto it = my_list.insert(my_list.begin(), 1); // Вставляем 1 в пустой список
+    EXPECT_EQ(1, *my_list.begin());  // Проверяем, что список теперь содержит один элемент
+    EXPECT_EQ(1, *it);  // Возвращаемый итератор должен указывать на новый элемент
+    EXPECT_EQ(1, my_list.size());  // Размер списка должен быть 1
+}
+
+// Тест на удаление элемента из списка
+TEST(ListTest, ErasesElement) {
+    list<int> my_list{1, 2, 3};
+    auto it = my_list.begin();
+    ++it; // Указываем на элемент со значением 2
+    my_list.erase(it);
+
+    // Проверяем, что элемент 2 был удален
+    int expected[] = {1, 3};
+    int i = 0;
+    for (auto elem : my_list) {
+        EXPECT_EQ(expected[i++], elem);
+    }
+}
+
+// Тест на добавление элемента в конец списка
+TEST(ListTest, PushesBackElement) {
+    list<int> my_list;
+    my_list.push_back(1);
+    EXPECT_EQ(1, my_list.back());
+}
+
+// Тест на добавление элемента в начало списка
+TEST(ListTest, PushesFrontElement) {
+    list<int> my_list;
+    my_list.push_front(1);
+    EXPECT_EQ(1, my_list.front());
+}
+
+// Тест на удаление элемента из конца списка
+TEST(ListTest, PopsBackElement) {
+    list<int> my_list{1, 2, 3};
+    my_list.pop_back();
+    EXPECT_EQ(2, my_list.back());
+}
+
+// Тест на удаление элемента из начала списка
+TEST(ListTest, PopsFrontElement) {
+    list<int> my_list{1, 2, 3};
+    my_list.pop_front();
+    EXPECT_EQ(2, my_list.front());
+}
+
+// Тест на обмен содержимым списков
+TEST(ListTest, SwapsLists) {
+    list<int> my_list1{1, 2, 3};
+    list<int> my_list2{4, 5, 6};
+    my_list1.swap(my_list2);
+
+    EXPECT_EQ(4, my_list1.front());
+    EXPECT_EQ(3, my_list2.front());
+}
+
+// Тест на слияние двух списков
+TEST(ListTest, MergesLists) {
+    list<int> my_list1{1, 3, 5};
+    list<int> my_list2{2, 4, 6};
+    my_list1.merge(my_list2);
+
+    // Проверяем, что my_list2 теперь пустой
+    EXPECT_TRUE(my_list2.empty());
+
+    // Проверяем, что элементы в my_list1 теперь идут по порядку
+    int expected[] = {1, 2, 3, 4, 5, 6};
+    int i = 0;
+    for (auto elem : my_list1) {
+        EXPECT_EQ(expected[i++], elem);
+    }
+}
+
+// Тест на переворот списка
+TEST(ListTest, ReversesList) {
+    list<int> my_list{1, 2, 3};
+    my_list.reverse();
+
+    int expected[] = {3, 2, 1};
+    int i = 0;
+    for (auto elem : my_list) {
+        EXPECT_EQ(expected[i++], elem);
+    }
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
